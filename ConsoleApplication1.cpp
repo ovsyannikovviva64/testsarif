0981c74adce9b01209ea14d8ca1b52c52a92ef3d
Eugene Ovsyannikov <ovsyannikov@ad.viva64.com>
2020-09-10 14:15:57 +0300

    first result7

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -7,8 +7,8 @@ int main()
 {
     std::cout << "Hello World!\n";
     int a = 0; ddd
-    a = a; // ok or not dddd bla bla
-} //
+    a = a; // ok or not dddd bla bla xxx
+} // end
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
